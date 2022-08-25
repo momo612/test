02da055805502175338b99b785b053830f8e6e41
莫泽云 <mozeyun.auteltech.net>
2022-08-25 15:14:28 +0800

    修改提交描述信息

--- service/src/main/java/com/example/service/controller/HelloWorldController.java
+++ service/src/main/java/com/example/service/controller/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/helloWorld")
     public String HelloWorld(String name){
-        String s = "hello " + name;
+        String s = "hello11 " + name;
         System.out.println(s);
         return s;
     }
